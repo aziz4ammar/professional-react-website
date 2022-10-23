17793117161d68e089217ce3e9f7cf2cb07010fc
mohamed aziz ammar <azizammar67@gmail.com>
2022-10-23 18:47:44 +0100

    commit number #5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CollapsibleExample from './Components/Navbar/Headder';
+import Body from './Components/Body/Body';
 function App() {
   return (
     <div className="App">
       <CollapsibleExample/>
+      <Body/>
     </div>
   );
 }
