f486962d6ab5035372cb403d0b84c793f0e85271
mohamed aziz ammar <azizammar67@gmail.com>
2022-10-24 20:23:23 +0100

    commit number #8

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,5 +1,5 @@
 export const tab = [
     {id:Math.random() , image:"https://auspost.com.au/shop/static/WFS/AusPost-Shop-Site/-/AusPost-Shop/en_AU/product/059069724/1/org.png",
 name:"Google Play Gift Card",Price:10,cont:1,
-},
+}
 ]
