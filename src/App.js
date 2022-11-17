c44586c46b026841769d76b8e96b2d6a932bf922
mohamed aziz ammar <azizammar67@gmail.com>
2022-11-17 18:19:51 +0100

    adding image

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import CollapsibleExample from './Components/Navbar/Headder';
 import Body from './Components/Body/Body';
 import { useState } from 'react';
 import { tab } from './Data';
+import Affiche from './Components/Body/Affiche';
 function App() {
   const [info, setInfo] = useState(tab)
   return (
     <div className="App">
       <CollapsibleExample/>
+      <Affiche/>
       <div className='ab'>{info.map((el,i)=><Body el={el} key={i}/>)}</div>
     </div>
   );
--- src/Components/Body/Affiche.jsx
+++ src/Components/Body/Affiche.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import as from "../img/as.jpg";
 const Affiche = () => {
   return (
     <div>
-        <img className='img' src="" alt="" />
+        <img className='img' src={} alt="" />
         <h1></h1>
         <p></p>
     </div>
