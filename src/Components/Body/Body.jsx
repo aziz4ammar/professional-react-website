280b1c7184b09afe75115b9097606fdb803e18fd
mohamed aziz ammar <azizammar67@gmail.com>
2022-11-28 19:02:58 +0100

    commit number #39

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ar from "../img/aqq.jpg";
 import "./body.css"
 const Body = ({el}) => {
     return (
             <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={el.image} />
+              <Card.Img variant="top" src={ar} />
               <Card.Body>
                 <Card.Title>{el.name}</Card.Title>
                 <Card.Text>
