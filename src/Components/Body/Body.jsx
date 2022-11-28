6f47ad44eabee1ede238ed7441de148af99222c1
mohamed aziz ammar <azizammar67@gmail.com>
2022-11-28 19:03:53 +0100

    commit number #40

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import ar from "../img/aqq.jpg";
 import "./body.css"
 const Body = ({el}) => {
     return (
             <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={ar} />
+              <Card.Img variant="top" src={el.image} />
               <Card.Body>
                 <Card.Title>{el.name}</Card.Title>
                 <Card.Text>
--- src/Data.jsx
+++ src/Data.jsx
@@ -1,11 +1,11 @@
 export const tab = [
-//     {id:Math.random() , image:"",
-// name:"title",text:"texttttttttttttt",Price:10,cont:1,
-// },
+    {id:Math.random() , image:"",
+name:"title",text:"texttttttttttttt",Price:10,cont:1,
+},
 {id:Math.random() , image:"",
 name:"title",text:"texttttttttttttt",Price:10,cont:1,
 },
-// {id:Math.random() , image:"",
-// name:"title",text:"texttttttttttttt",Price:10,cont:1,
-// }
+{id:Math.random() , image:"",
+name:"title",text:"texttttttttttttt",Price:10,cont:1,
+}
 ]
