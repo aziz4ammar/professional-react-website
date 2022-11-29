be8705ad50147c31c2b48592b3048580d27b8883
mohamed aziz ammar <azizammar67@gmail.com>
2022-11-29 20:07:38 +0100

    commit number #42

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,11 +1,11 @@
 export const tab = [
     {id:Math.random() , image:"https://s13emagst.akamaized.net/products/50852/50851521/images/res_23db1c5f1f16fb8c09755551c726fd04.jpg?width=450&height=450&hash=C23F38427C4202159CEB441BA60F36FA",
-// name:"iPhone 14 Pro Max",text:"128GB-Black",Price:10,cont:1,
+name:"iPhone 14 Pro Max",text:"128GB-Black",Price:10,cont:1,
 },
 {id:Math.random() , image:"https://s13emagst.akamaized.net/products/48592/48591224/images/res_c5366b309c505a8dc1361685a1c2ed9d.jpg?width=450&height=450&hash=28F8783F527C6E5F9F43DC1FFE81A802",
-// name:"iPhone 14 Pro Max",text:"128GB-Gold",Price:10,cont:1,
+name:"iPhone 14 Pro Max",text:"128GB-Gold",Price:10,cont:1,
 },
 {id:Math.random() , image:"https://s13emagst.akamaized.net/products/48592/48591241/images/res_427992596019b4690e24dcda076fff6a.jpg?width=450&height=450&hash=A7B80170EC117972647BA60E646C15E0",
-// name:"iPhone 14 Pro Max",text:"128GB-Silver",Price:10,cont:1,
+name:"iPhone 14 Pro Max",text:"128GB-Silver",Price:10,cont:1,
 }
 ]
