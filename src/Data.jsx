59c555d73b3d1712b5275aea889862ccaa5c049e
mohamed aziz ammar <azizammar67@gmail.com>
2022-12-31 23:25:49 +0100

    commit number #94

--- src/Data.jsx
+++ src/Data.jsx
@@ -44,7 +44,7 @@ name:"iPhone 13 pro",text:"128GB-Gold",Price:10,cont:1,
 {id:Math.random() , image:"https://s13emagst.akamaized.net/products/40685/40684397/images/res_9aa260aaaa84abf7f2a3db2bd2f85a8e.jpg?width=450&height=450&hash=E0DAEFC55242E20912D93D61E9D3C782",
 name:"iPhone 13 pro",text:"128GB-Silver",Price:10,cont:1,
 },
-{id:Math.random() , image:"https://s13emagst.akamaized.net/products/40685/40684397/images/res_9aa260aaaa84abf7f2a3db2bd2f85a8e.jpg?width=450&height=450&hash=E0DAEFC55242E20912D93D61E9D3C782",
+{id:Math.random() , image:"https://s13emagst.akamaized.net/products/40685/40684394/images/res_bc4f6a2aadf693275df5a1aea2c32d1e.jpg?width=450&height=450&hash=43D18C25CEB4CED672DCDF5185F10748",
 name:"iPhone 13 pro",text:"128GB-Sierra Blue",Price:10,cont:1,
 },
 ]
